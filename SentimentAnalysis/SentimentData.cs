210be4269d068d9cf88329496041f830f5285cca
NonOxC <59771975+NonOxC@users.noreply.github.com>
2025-01-16 23:51:01 -0500

    Add files via upload

---
+++ SentimentAnalysis/SentimentData.cs
@@ -0,0 +1,31 @@
+﻿// <SnippetAddUsings>
+using Microsoft.ML.Data;
+// </SnippetAddUsings>
+
+namespace SentimentAnalysis
+{
+    // <SnippetDeclareTypes>
+    public class SentimentData
+    {
+        [LoadColumn(0), ColumnName("Label")] 
+        public bool Sentiment { get; set; }
+
+        [LoadColumn(1)] 
+        public string Title { get; set; }
+
+        [LoadColumn(2)] 
+        public string SentimentText { get; set; }
+    }
+
+    public class SentimentPrediction : SentimentData
+    {
+
+        [ColumnName("PredictedLabel")]
+        public bool Prediction { get; set; }
+
+        public float Probability { get; set; }
+
+        public float Score { get; set; }
+    }
+    // </SnippetDeclareTypes>
+}
